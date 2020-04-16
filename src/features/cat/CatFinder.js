294693cf1b5ac8b2e281835d9cf77325e02d4572
Willian Watanabe <watinha@gmail.com>
2020-04-16 19:18:04 -0300

    using cat favorite list funcionality

--- src/features/cat/CatFinder.js
+++ src/features/cat/CatFinder.js
@@ -2,17 +2,30 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function CatFinder () {
-    const [current, setCurrent] = useState('');
+    const [current, setCurrent] = useState(''),
+          [favorite, setFavorite] = useState([]);
 
     async function find_cat () {
         const response = await axios.get('https://aws.random.cat/meow');
         setCurrent(response.data.file);
     }
 
+    function like () {
+        setFavorite([current, ...favorite]);
+        find_cat();
+    }
+
     return (
         <div className="cat_finder">
-            <div><img src={current} /></div>
+            <ul>
+            {favorite.map((f, index) => {
+                return <li key={index}><img height="100" src={f}/></li>
+            })}
+            </ul>
+
+            <div><img height="300" src={current} /></div>
             <button onClick={() => find_cat()}>Carregar gato...</button>
+            <button onClick={() => like()}>Like!</button>
         </div>
     );
 }
